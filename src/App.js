3f2fbc7ee04af645fde1f1589c1bd281e7535beb
haraphat01 <haraphat@gmail.com>
2022-09-22 08:35:57 +0100

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 
 import { useState, useEffect } from 'react';
 import './App.css';
-const te = require('tradingeconomics');
-te.login(process.env.REACT_APP_API);
+
 
 
 function App() {
-const [datas, setDatas] = useState([])
-let country;
-  useEffect(()=>{
-    let data = te.getIndicatorData(country = ['mexico']).then(function(data){
-      setDatas( data)
-    });
-   
-  }); 
-  
 
-  console.log(datas)
 
 
   return (
     <div className="App">
-     <p>Economic Data</p>
-     {datas.map(item => {
-       return(
-         <p>{item[0].Category}</p>
-       )
-     })}
+      <Sidebar />
+        <Main />
     </div>
   );
 }
