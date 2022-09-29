f3f672ed4ac47509ec9e6175a6c5280ecd4c4737
haraphat01 <haraphat@gmail.com>
2022-09-29 09:17:47 +0100

    Remodelled the state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 
-import {useEffect, React, useReducer } from 'react';
-import axios from 'axios';
+// import {useEffect, React, useReducer } from 'react';
+// import axios from 'axios';
 import './App.css';
 import Main from './Main';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
 
 function App() {
-  
+  let counter = useSelector((state) => state.counter)
+  let dispatch = useDispatch
+  function inc(){}
+  function dec(){}
 
   return (
     <div className="App">
       
       <Main/>
+      <h1>Counter</h1>
+      <p>{counter}</p>
+      <button onClick={inc}> inc</button>
+      <button onCanPlay={dec}>dec</button>
    
 
        </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,12 @@
-import { createStore } from "redux";
-const store = createStore(reducer, {contacts});
-export default store;
+import {createStore} from 'redux'
+
+
+const reducerFn = (state = {counter: 0}, action)=>{
+    if (action.type === "INC"){
+        return {...state, state: state.counter + 1 }
+    } else if (action.type === "DEC"){
+        return {...state, state: state.counter - 1 }
+    }
+}
+const store = createStore(reducerFn)
+export default store
